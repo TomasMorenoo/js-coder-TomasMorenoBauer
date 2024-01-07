0aaf5b842cce5ba0ec512e570ddf04bbaf551e92
Tomas Moreno Bauer <MorenoBauer10@gmail.com>
2024-01-07 00:45:47 -0300

    ya no se que nombre ponerle a los commit

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ function generateProductGrid(products) {
         productName.classList.add('card-title');
         productName.textContent = product.name;
 
-        // Agregar evento de clic al título para redireccionar a la página "producto.html"
         productName.addEventListener('click', function() {
             window.location.href = `producto.html?id=${product.id}`;
         });
@@ -64,14 +63,14 @@ function generateProductGrid(products) {
 }
 
 const products = [
-    { id: 1, name: 'Pack x3 medias Nike', image: 'assets/fotos/productos/medias1.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
-    { id: 2, name: 'Reloj Inteligente', image: 'assets/fotos/productos/medias1.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
-    { id: 3, name: 'Zapatillas Deportivas', image: 'assets/fotos/productos/medias1.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
-    { id: 4, name: 'Reloj Inteligente', image: 'assets/fotos/productos/medias1.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
-    { id: 5, name: 'Pack x3 medias Nike', image: 'assets/fotos/productos/medias1.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
-    { id: 6, name: 'Reloj Inteligente', image: 'assets/fotos/productos/medias1.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
-    { id: 7, name: 'Zapatillas Deportivas', image: 'assets/fotos/productos/medias1.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
-    { id: 8, name: 'Reloj Inteligente', image: 'assets/fotos/productos/medias1.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
+    { id: 1, name: 'Pack x3 medias surtidas Nike ', image: 'assets/fotos/productos/medias1.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
+    { id: 2, name: 'Pelota adidas Al Rihla Pro', image: 'assets/fotos/productos/pelota.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
+    { id: 3, name: 'Remera Urbana adidas Dream Doodle ', image: 'assets/fotos/productos/remera.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
+    { id: 4, name: 'Pelota Básquet Spalding Varsity número 7', image: 'assets/fotos/productos/SG845128-1.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
+    { id: 1, name: 'Pack x3 medias surtidas Nike', image: 'assets/fotos/productos/medias1.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
+    { id: 2, name: 'Pelota adidas Al Rihla Pro', image: 'assets/fotos/productos/pelota.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
+    { id: 3, name: 'Remera Urbana adidas Dream Doodle', image: 'assets/fotos/productos/remera.jpg', price: '$49.99', details: 'Zapatillas deportivas cómodas y elegantes.' },
+    { id: 4, name: 'Pelota Básquet Spalding Varsity número 7', image: 'assets/fotos/productos/SG845128-1.jpg', price: '$99.99', details: 'Reloj inteligente con funciones avanzadas.' },
 ];
 
 generateProductGrid(products);
